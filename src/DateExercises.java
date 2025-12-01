ecd3b25b9f34e0769168803385203f920bf2a5f8
Dragos Popescu <dragospopescu74@yahoo.com>
2025-12-01 15:27:40 +0200

    Added DateExercises

---
+++ src/DateExercises.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class DateExercises {
+    public static void howMuchTime(LocalDate past){
+        if (past.isAfter(LocalDate.now())){
+            System.out.println("This date is in the future");
+        }
+
+        LocalDate now = LocalDate.now();
+
+        Period period = Period.between(past, now);
+        long days = ChronoUnit.DAYS.between(past, now);
+
+        System.out.printf("Current date is: " + now);
+        System.out.printf("\nPeriod is: " + " \n "+
+                " Days: " + period.getDays()+"\n "+
+                " Moths: " + period.getMonths()+" \n "+
+                " Years: " + period.getYears());
+        System.out.printf("\nDays between past date and current date: " + days);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -5,18 +6,20 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int [] arr1 = new int [5];
-        int [] arr2 = {7,5,33,4,55};
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array elements: ");
-        arr1[0] = sc.nextInt();
-        arr1[1] = sc.nextInt();
-        arr1[2] = sc.nextInt();
-        arr1[3] = sc.nextInt();
-        arr1[4] = sc.nextInt();
-        System.out.printf("The formatted array1 is: "+ ArraysUtils.intToArray(arr1));
-        System.out.println("\nThe formatted array2 is: "+ Arrays.toString(arr2));
+//        int [] arr1 = new int [5];
+//        int [] arr2 = {7,5,33,4,55};
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter array elements: ");
+//        arr1[0] = sc.nextInt();
+//        arr1[1] = sc.nextInt();
+//        arr1[2] = sc.nextInt();
+//        arr1[3] = sc.nextInt();
+//        arr1[4] = sc.nextInt();
+//        System.out.printf("The formatted array1 is: "+ ArraysUtils.intToArray(arr1));
+//        System.out.println("\nThe formatted array2 is: "+ Arrays.toString(arr2));
 
+        LocalDate past =  LocalDate.of(2005,05,12);
+        DateExercises.howMuchTime(past);
 
         }
     }
